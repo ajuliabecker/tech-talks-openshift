a9d0d0c3bd2ffc80a8c0e923a8b3f36c084c2065
Júlia <ajuliabecker@gmail.com>
2020-04-30 03:18:55 -0300

    change port

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app =  Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "TECH TALKS - Openshift"
 
 if __name__ == "__main__":  
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0")
